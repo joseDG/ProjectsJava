d11e4b479f06fafce56cd10bc01474bde706ba04
Jose <josethp1@gmail.com>
2021-12-17 15:21:31 -0500

    Finish section BloquesCodigo

---
+++ 26.BloquesDeCodigo/src/domain/testBloquesIniciales.java
@@ -0,0 +1,16 @@
+
+package domain;
+
+/**
+ *
+ * @author Josh
+ */
+public class testBloquesIniciales {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        System.out.println("Persona 1" + persona1);
+        System.out.println("");
+        Persona persona2 = new Persona();
+        System.out.println("Persona 2" + persona2);
+    }
+}
